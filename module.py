b9510ed5b05865550eddce876db18b0cefee9952
Cèsar Jaldin <rodia.piedra@gmail.com>
2019-11-13 21:40:09 -0400

    works from class 13/11

---
+++ module.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" module.py - an example of Python module """
+
+__counter = 0
+
+def sum1(list):
+  global __counter
+  __counter += 1
+  sum = 0
+  for el in list:
+    sum += el
+  return sum
+
+def prod1(list):
+  global __counter
+  __counter += 1
+  prod = 1
+  for el in list:
+    prod *= el
+  return prod
+
+if __name__ == "__main__":
+  print("I prefer to be a module, but I can do some tests for you")
+  l = [i+1 for i in range(5)]
+  print(sum1(l) == 15)
+  print(prod1(l) == 120)
